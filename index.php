ac3bc73bfaff7b661be0774a8b9088d9ce331525
Nurul Fatanah <42891145+nfhatanah@users.noreply.github.com>
2022-07-01 11:45:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Landing Page</title>
+<style type="text/css">
+body {
+	background-image: url(/pbmsmada/image/bg/bg1.jpg);
+	background-repeat: no-repeat;
+	color: #FFFFFF;
+	font-size: 16px;
+	text-align: center;
+}
+#form1 {
+	font-family: "Microsoft New Tai Lue";
+	font-size: 70px;
+	font-weight: bold;
+	text-align: center;
+	color: #FFF;
+}
+#form2 p {
+	font-family: "Microsoft New Tai Lue";
+	font-size: 47px;
+	font-weight: bold;
+	line-height: 10px;
+	color: #FF0;
+	text-align: center;
+}
+#form3 {
+	font-size: 40px;
+	font-family: Verdana, Geneva, sans-serif;
+	color: #000;
+	font-weight: bold;
+	text-align: center;
+}
+#form4 {
+	background-color: #006;
+}
+#form5 {
+	font-size: 12px;
+}
+#form4 marquee {
+	font-size: 36px;
+}
+</style>
+<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onload="MM_preloadImages('/pbmsmada/image/bg/logoadminafter.png','/pbmsmada/image/bg/logoatasanafter.png','/pbmsmada/image/bg/loginguruafter.png')">
+<table width="1332" border="0" cellspacing="3">
+  <tr>
+    <td width="273" rowspan="3" bgcolor="#000066"><img src="/pbmsmada/image/bg/logosmada.png" width="226" height="226" /></td>
+    <td width="1080" height="60" nowrap="nowrap" bgcolor="#000066"><form id="form1" name="form1" method="post" action="">
+      SELAMAT DATANG
+    DI
+    </form></td>
+  </tr>
+  <tr>
+    <td width="1080" height="60" bgcolor="#990000"><form id="form2" name="form2" method="post" action="">
+      <p>Sistem Pelaporan</p>
+      <p>Proses Belajar Mengajar Harian Guru</p>
+    </form></td>
+  </tr>
+  <tr>
+    <td width="1080" height="50" bgcolor="#FFFFFF"><form id="form3" name="form3" method="post" action="">
+      SMA Negeri 2 Polewali
+    </form></td>
+  </tr>
+</table>
+<table width="1332" border="0" align="left" cellspacing="3">
+  <tr>
+    <td width="269" bgcolor="#000066"><form id="form4" name="form4" method="post" action=""><marquee scrolldelay="250">
+      SMA Negeri 2 Polewali : Jl. Hos Cokroaminoto, Kel. Pekkabata, Kec. Polewali, Kab. Polewali Mandar, Prov. Sulawesi Barat, 91311
+    </marquee></form></td>
+    <td width="1050" rowspan="3"><table width="659" border="0" align="center" cellspacing="0">
+      <tr>
+        <td width="180" height="249"><p><a href="/pbmsmada/admin/loginadmin/selamatdatangadmin.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('loginadmin','','/pbmsmada/image/bg/logoadminafter.png',1)"><img src="/pbmsmada/image/bg/logoadmin - Copy.png" alt="" name="loginadmin" width="169" height="170" id="loginadmin" /></a>Admin</p></td>
+        <td width="50"><a href="/pbmsmada/loginatasan.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('loginatasan','','/pbmsmada/image/bg/logoatasanafter.png',1)"></a></td>
+        <td width="180"><p><a href="/pbmsmada/atasan/loginatasan/selamatdatangatasan.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('loginatasan','','/pbmsmada/image/bg/logoatasanafter.png',1)"><img src="/pbmsmada/image/bg/logoatasancopy.png" alt="" name="loginatasan" width="169" height="169" id="loginatasan" /></a>Atasan</p></td>
+        <td width="50">&nbsp;</td>
+        <td width="177"><p><a href="/pbmsmada/admin/isiadmin/selamatdatangguru.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('loginguru','','/pbmsmada/image/bg/loginguruafter.png',1)"><img src="/pbmsmada/image/bg/loginguru.png" alt="" name="loginguru" width="170" height="169" id="loginguru" /></a>Guru</p></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="186" bgcolor="#000066"><img src="/pbmsmada/image/bg/libros.png" alt="" width="237" height="237" /></td>
+  </tr>
+  <tr>
+    <td height="32" bgcolor="#000066"><form id="form5" name="form5" method="post" action="">
+      Powered By Nurul Fatanah (D0216033) Teknik Informatika - Universitas Sulawesi Barat
+    </form></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
